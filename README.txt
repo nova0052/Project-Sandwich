5b1e42c563b8dcf2d6f350c247dc0ca3fb1cc7b4
nova0052 <nova0052@gmail.com>
2015-08-28 17:41:49 -0700

    Loading up the first lovely snippet for all to see:
    "Wow Nova is such a n00b"
    
    "This guy sucks at Python"
    
    "He should leave Sandwich making to the pros"

---
+++ README.txt
@@ -0,0 +1,5 @@
+This is the readme file.
+
+all of this code is super top secret calcified type stuff.
+
+Thanks!
---
+++ controltest-analog.py
@@ -0,0 +1,53 @@
+import pygame, sys
+from pygame.locals import *
+
+# define variables
+green = (  0, 255,   0)
+red   = (255,   0,   0)
+blue  = (  0,   0, 255)
+black = (  0,   0,   0)
+white = (255, 255, 255)
+
+# draw the screen
+Screen = pygame.display.set_mode((800, 600))
+Screen.fill(black)
+
+# draw the joystick input area
+stick_box = pygame.Rect(50, 150, 300, 300)
+pygame.draw.rect(Screen, green, stick_box, 0)
+pygame.draw.circle(Screen, blue, (200, 300), 10, 0)
+# draw the motor output area
+motorL = pygame.Rect(500, 150, 75, 300)
+pygame.draw.rect(Screen, green, motorL, 0)
+motorR = pygame.Rect(625, 150, 75, 300)
+pygame.draw.rect(Screen, green, motorR, 0)
+
+# define functions
+def terminate():
+    pygame.quit()
+    sys.exit()
+    
+# set up joystick
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init
+
+# make a loop to hold everything
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            terminate()
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                terminate()
+
+# get joystick inputs and animate the screen
+        elif event.type == JOYAXISMOTION:
+            x_val = -(joystick.get_axis(0))
+            y_val = joystick.get_axis(1)
+            pygame.joystick.get_axis(0)
+            pygame.joystick.get_axis(1)
+            pygame.draw.circle(Screen, blue, ((x_val * 150 + 50), (y_val * 150 + 150)), 10, 0)
+
+# close the loop
+    pygame.display.update()
